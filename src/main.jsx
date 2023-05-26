413b88d79e99ab6e333fa3f374547406bd7c4f53
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-25 21:26:10 -0700

    Set Root as root route element

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
