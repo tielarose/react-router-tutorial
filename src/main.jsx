284ff2d3c4a95de129f73b3aa4772cf59b193816
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-25 21:41:55 -0700

    Nest routes and use Link for client-side routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contacts";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Root />, errorElement: <ErrorPage /> }
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "contacts/:contactsId", element: <Contact /> }]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
