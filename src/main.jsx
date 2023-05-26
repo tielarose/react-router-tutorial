d2a252c5ec01058ba686cdbaac42da54299cdaed
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-25 21:34:14 -0700

    Create ErrorPage element

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
-const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Root />, errorElement: <ErrorPage /> }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
